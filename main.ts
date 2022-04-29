1d332f6fb83772f6d14660ad9af3a14d312e83f8
chzarafidis <45843987+chzarafidis@users.noreply.github.com>
2022-04-29 14:27:47 +0300

    Ενημέρωση pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(500)
+    kitronik_servo_lite.forward()
+    basic.pause(1000)
+    kitronik_servo_lite.stop()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(500)
+    servos.P0.setAngle(20)
+    basic.pause(1000)
+    servos.P0.setAngle(-20)
+    kitronik_servo_lite.stop()
+})
 basic.forever(function () {
 	
 })
